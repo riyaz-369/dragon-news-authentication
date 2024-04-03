52723cdd80b88edaf52fdbb1ecc23f6b077eda1f
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-03 21:34:00 +0600

    Custom Tailwind Class And Font Setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h2 className='text-3xl'>This is Home</h2>
+            <h2 className='text-3xl font-poppins'>This is Home</h2>
         </div>
     );
 };
