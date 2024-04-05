6afbcde73f80f3ce24226de4127db162058e83f5
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-04-05 14:49:59 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDjEszqek1pjJgNQmOXcQGP_DNqyC_koGs",
-  authDomain: "dragon-news-authenticati-26604.firebaseapp.com",
-  projectId: "dragon-news-authenticati-26604",
-  storageBucket: "dragon-news-authenticati-26604.appspot.com",
-  messagingSenderId: "1048900194747",
-  appId: "1:1048900194747:web:65883fd06e35fc2564749e",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
